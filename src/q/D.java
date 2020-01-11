33daacd440eb60ca711a68356c58c31b6339baad
srigowri-08 <54535869+srigowri-08@users.noreply.github.com>
2020-01-11 13:22:57 +0530

    Update D.java

--- src/q/D.java
+++ src/q/D.java
@@ -4,6 +4,8 @@ public class D {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello merge my branch with master");
+		System.out.println("hello merge my branch with master");
 
 	}
 
